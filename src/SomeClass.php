4ea923227ea10ba4f8f2e7598d08dbba3d32cadd
marlon <kontakt@marlonschultz.de>
2018-11-17 00:53:33 +0100

    first commit

---
+++ src/SomeClass.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: mgbs
+ * Date: 17.11.18
+ * Time: 00:48
+ */
+
+class SomeClass
+{
+
+}
---
+++ tests/SomeClassTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mgbs
+ * Date: 17.11.18
+ * Time: 00:48
+ */
+
+
+class SomeClassTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSomethingAndFail()
+    {
+        $this->assertTrue(false);
+    }
+}
